7895f3344db093f46e175d0af2f4acdcec2b2e7d
sd <efd>
2017-04-30 20:24:53 +0300

    localization

---
+++ src/main/java/com/itransition/portfl/controller/LocalizationController.java
@@ -0,0 +1,37 @@
+package com.itransition.portfl.controller;
+
+import com.itransition.portfl.localization.LocalizationInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Kulik Artur
+ */
+@CrossOrigin
+@RestController
+@RequestMapping(value = "localization")
+public class LocalizationController {
+
+    private LocalizationInterface ruLocalization;
+    private LocalizationInterface enLocalization;
+
+    @Autowired
+    public LocalizationController( @Qualifier("localizationRU") LocalizationInterface ruLocalization,
+                                   @Qualifier("localizationEN") LocalizationInterface enLocalization){
+        this.ruLocalization = ruLocalization;
+        this.enLocalization = enLocalization;
+    }
+
+    @GetMapping(value = "get")
+    public ResponseEntity<?> getLocal(@RequestParam(value = "name") String name){
+        return ResponseEntity.ok(this.getConcretLocal(name).getLocal());
+    }
+
+    private LocalizationInterface getConcretLocal(String name) {
+        if(name.equals("ru")) return this.ruLocalization;
+        return this.enLocalization;
+    }
+
+}
---
+++ src/main/java/com/itransition/portfl/localization/LocalizationInterface.java
@@ -0,0 +1,11 @@
+package com.itransition.portfl.localization;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Kulik Artur
+ */
+@Component
+public interface LocalizationInterface {
+    String getLocal();
+}
